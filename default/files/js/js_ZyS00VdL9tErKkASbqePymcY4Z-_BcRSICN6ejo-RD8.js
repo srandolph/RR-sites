e71ff282b14a4715382b4083d586fd7ddacb1f76
Scott Randolph <srandolph@tetrasys.org>
2014-10-29 22:16:22 -0500

    Ron's update 20141028

---
+++ default/files/js/js_ZyS00VdL9tErKkASbqePymcY4Z-_BcRSICN6ejo-RD8.js
@@ -0,0 +1,352 @@
+(function ($) {
+  Drupal.behaviors.ws_lsb = {
+    attach: function (context, settings) {
+      $('a.service-links-linkedin-share-button', context).each(function(){
+        var script_obj = document.createElement('script');
+        script_obj.type = 'IN/Share';
+        script_obj.setAttribute("data-url", $(this).attr('href'));
+        if (Drupal.settings.ws_lsb.countmode != '') {
+          script_obj.setAttribute("data-counter", Drupal.settings.ws_lsb.countmode);
+        }
+        $(this).replaceWith(script_obj);
+      });
+
+      try {
+        IN.init({
+          onLoad: "Drupal.behaviors.ws_lsb.parse"
+        });
+      }
+      catch(e) {
+        if (window.console && window.console.log) {
+          console.log(e);
+        }
+      }
+    },
+
+    parse: function(context) {
+      try {
+        IN.parse(context);
+      }
+      catch(e) {
+        if (window.console && window.console.log) {
+          console.log(e);
+        }
+      }
+    }
+  }
+})(jQuery);
+;
+/**
+ * @file
+ * JavaScript file for the Coffee module.
+ */
+
+(function($) {
+  // Remap the filter functions for autocomplete to recognise the
+  // extra value "command".
+  var proto = $.ui.autocomplete.prototype,
+  	initSource = proto._initSource;
+
+  function filter(array, term) {
+  	var matcher = new RegExp( $.ui.autocomplete.escapeRegex(term), 'i');
+  	return $.grep(array, function(value) {
+                return matcher.test(value.command) || matcher.test(value.label) || matcher.test(value.value);
+  	});
+  }
+
+  $.extend(proto, {
+  	_initSource: function() {
+  		if ($.isArray(this.options.source)) {
+  			this.source = function(request, response) {
+  				response(filter(this.options.source, request.term));
+  			};
+  		}
+  		else {
+  			initSource.call(this);
+  		}
+  	}
+  });
+
+  Drupal.coffee = Drupal.coffee || {};
+
+  Drupal.behaviors.coffee = {
+    attach: function() {
+      $('body').once('coffee', function() {
+        var body = $(this);
+
+        Drupal.coffee.bg.appendTo(body).hide();
+
+        Drupal.coffee.form
+        .append(Drupal.coffee.label)
+        .append(Drupal.coffee.field)
+        .append(Drupal.coffee.results)
+        .wrapInner('<div id="coffee-form-inner" />')
+        .addClass('hide-form')
+        .appendTo(body);
+
+        // Load autocomplete data set, consider implementing
+        // caching with local storage.
+        Drupal.coffee.dataset = [];
+        
+        var jquery_ui_version = $.ui.version.split('.');
+        var jquery_ui_newer_1_9 = parseInt(jquery_ui_version[0]) >= 1 && parseInt(jquery_ui_version[1]) > 9;
+        var autocomplete_data_element = (jquery_ui_newer_1_9) ? 'ui-autocomplete' : 'autocomplete';
+
+        $.ajax({
+          url: Drupal.settings.basePath + '?q=admin/coffee/menu',
+          dataType: 'json',
+          success: function(data) {
+            Drupal.coffee.dataset = data;
+
+            // Apply autocomplete plugin on show
+            var $autocomplete = $(Drupal.coffee.field).autocomplete({
+              source: Drupal.coffee.dataset,
+              focus: function( event, ui ) {
+                  // Prevents replacing the value of the input field
+                  event.preventDefault();
+              },
+              select: function(event, ui) {
+                Drupal.coffee.redirect(ui.item.value, event.metaKey);
+                event.preventDefault();
+                return false;
+              },
+              delay: 0,
+              appendTo: Drupal.coffee.results
+           });
+
+           $autocomplete.data(autocomplete_data_element)._renderItem = function(ul, item) {
+              return  $('<li></li>')
+                      .data('item.autocomplete', item)
+                      .append('<a>' + item.label + '<small class="description">' + item.value + '</small></a>')
+                      .appendTo(ul);
+            };
+
+            // This isn't very nice, there are methods within that we need
+            // to alter, so here comes a big wodge of text...
+            var self = Drupal.coffee.field;
+            if (!jquery_ui_newer_1_9){
+                $(Drupal.coffee.field).data(autocomplete_data_element).menu = $('<ol></ol>')
+                    .addClass('ui-autocomplete')
+                    .appendTo(Drupal.coffee.results)
+                    // prevent the close-on-blur in case of a "slow" click on the menu (long mousedown).
+                    .mousedown(function(event) {
+                        event.preventDefault();
+                    })
+                    .menu({
+                        selected: function(event, ui) {
+                            var item = ui.item.data('item.autocomplete');
+                            Drupal.coffee.redirect(item.value, event.metaKey);
+                            event.preventDefault();
+                        }
+                    })
+
+                    .hide()
+                    .data('menu');
+            }
+
+            // We want to limit the number of results.
+            $(Drupal.coffee.field).data(autocomplete_data_element)._renderMenu = function(ul, items) {
+          		var self = this;
+          		items = items.slice(0, 7); // @todo: max should be in Drupal.settings var.
+          		$.each( items, function(index, item) {
+                    if (typeof(self._renderItemData) === "undefined"){
+                        self._renderItem(ul, item);
+                    }
+                    else {
+                        self._renderItemData(ul, item);
+                    }
+
+          		});
+          	};
+
+          	// On submit of the form select the first result if available.
+          	Drupal.coffee.form.submit(function() {
+          	  var firstItem = jQuery(Drupal.coffee.results).find('li:first').data('item.autocomplete');
+          	  if (typeof firstItem == 'object') {
+          	    Drupal.coffee.redirect(firstItem.value, false);
+          	  }
+
+          	  return false;
+          	});
+          },
+          error: function() {
+            Drupal.coffee.field.val('Could not load data, please refresh the page');
+          }
+        });
+
+        // Key events
+        $(document).keydown(function(event) {
+          var activeElement = $(document.activeElement);
+
+          // Show the form with alt + D. Use 2 keycodes as 'D' can be uppercase or lowercase.
+          if (Drupal.coffee.form.hasClass('hide-form') && 
+        		  event.altKey === true && 
+        		  // 68/206 = d/D, 75 = k. 
+        		  (event.keyCode === 68 || event.keyCode === 206  || event.keyCode === 75)) {
+            Drupal.coffee.coffee_show();
+            event.preventDefault();
+          }
+          // Close the form with esc or alt + D.
+          else if (!Drupal.coffee.form.hasClass('hide-form') && (event.keyCode === 27 || (event.altKey === true && (event.keyCode === 68 || event.keyCode === 206)))) {
+            Drupal.coffee.coffee_close();
+            event.preventDefault();
+          }
+        });
+      });
+    }
+  };
+
+  // Prefix the open and close functions to avoid
+  // conflicts with autocomplete plugin.
+
+  /**
+   * Open the form and focus on the search field.
+   */
+  Drupal.coffee.coffee_show = function() {
+    Drupal.coffee.form.removeClass('hide-form');
+    Drupal.coffee.bg.show();
+    Drupal.coffee.field.focus();
+    $(Drupal.coffee.field).autocomplete({enable: true});
+  };
+
+  /**
+   * Close the form and destroy all data.
+   */
+  Drupal.coffee.coffee_close = function() {
+    Drupal.coffee.field.val('');
+    //Drupal.coffee.results.empty();
+    Drupal.coffee.form.addClass('hide-form');
+    Drupal.coffee.bg.hide();
+    $(Drupal.coffee.field).autocomplete({enable: false});
+  };
+
+  /**
+   * Close the Coffee form and redirect.
+   * Todo: make it work with the overlay module.
+   */
+  Drupal.coffee.redirect = function(path, openInNewWindow) {
+    Drupal.coffee.coffee_close();
+
+    if (openInNewWindow) {
+      window.open(Drupal.settings.basePath + path);
+    }
+    else {
+      document.location = Drupal.settings.basePath + path;
+    }
+  };
+
+  /**
+   * The HTML elements.
+   */
+  Drupal.coffee.label = $('<label for="coffee-q" class="element-invisible" />').text(Drupal.t('Query'));
+
+  Drupal.coffee.results = $('<div id="coffee-results" />');
+
+  // Instead of appending results one by one, we put them in a placeholder element
+  // first and then append them all at once to prevent flickering while typing.
+  Drupal.coffee.resultsPlaceholder = $('<ol />');
+
+  Drupal.coffee.form = $('<form id="coffee-form" action="#" />');
+
+  Drupal.coffee.bg = $('<div id="coffee-bg" />').click(function() {
+    Drupal.coffee.coffee_close();
+  });
+
+  Drupal.coffee.field = $('<input id="coffee-q" type="text" autocomplete="off" />');
+
+}(jQuery));
+;
+(function ($) {
+
+$(document).ready(function() {
+
+  // Expression to check for absolute internal links.
+  var isInternal = new RegExp("^(https?):\/\/" + window.location.host, "i");
+
+  // Attach onclick event to document only and catch clicks on all elements.
+  $(document.body).click(function(event) {
+    // Catch the closest surrounding link of a clicked element.
+    $(event.target).closest("a,area").each(function() {
+
+      var ga = Drupal.settings.googleanalytics;
+      // Expression to check for special links like gotwo.module /go/* links.
+      var isInternalSpecial = new RegExp("(\/go\/.*)$", "i");
+      // Expression to check for download links.
+      var isDownload = new RegExp("\\.(" + ga.trackDownloadExtensions + ")$", "i");
+
+      // Is the clicked URL internal?
+      if (isInternal.test(this.href)) {
+        // Skip 'click' tracking, if custom tracking events are bound.
+        if ($(this).is('.colorbox')) {
+          // Do nothing here. The custom event will handle all tracking.
+        }
+        // Is download tracking activated and the file extension configured for download tracking?
+        else if (ga.trackDownload && isDownload.test(this.href)) {
+          // Download link clicked.
+          var extension = isDownload.exec(this.href);
+          _gaq.push(["_trackEvent", "Downloads", extension[1].toUpperCase(), this.href.replace(isInternal, '')]);
+        }
+        else if (isInternalSpecial.test(this.href)) {
+          // Keep the internal URL for Google Analytics website overlay intact.
+          _gaq.push(["_trackPageview", this.href.replace(isInternal, '')]);
+        }
+      }
+      else {
+        if (ga.trackMailto && $(this).is("a[href^='mailto:'],area[href^='mailto:']")) {
+          // Mailto link clicked.
+          _gaq.push(["_trackEvent", "Mails", "Click", this.href.substring(7)]);
+        }
+        else if (ga.trackOutbound && this.href.match(/^\w+:\/\//i)) {
+          if (ga.trackDomainMode == 2 && isCrossDomain(this.hostname, ga.trackCrossDomains)) {
+            // Top-level cross domain clicked. document.location is handled by _link internally.
+            event.preventDefault();
+            _gaq.push(["_link", this.href]);
+          }
+          else {
+            // External link clicked.
+            _gaq.push(["_trackEvent", "Outbound links", "Click", this.href]);
+          }
+        }
+      }
+    });
+  });
+
+  // Colorbox: This event triggers when the transition has completed and the
+  // newly loaded content has been revealed.
+  $(document).bind("cbox_complete", function() {
+    var href = $.colorbox.element().attr("href");
+    if (href) {
+      _gaq.push(["_trackPageview", href.replace(isInternal, '')]);
+    }
+  });
+
+});
+
+/**
+ * Check whether the hostname is part of the cross domains or not.
+ *
+ * @param string hostname
+ *   The hostname of the clicked URL.
+ * @param array crossDomains
+ *   All cross domain hostnames as JS array.
+ *
+ * @return boolean
+ */
+function isCrossDomain(hostname, crossDomains) {
+  /**
+   * jQuery < 1.6.3 bug: $.inArray crushes IE6 and Chrome if second argument is
+   * `null` or `undefined`, http://bugs.jquery.com/ticket/10076,
+   * https://github.com/jquery/jquery/commit/a839af034db2bd934e4d4fa6758a3fed8de74174
+   *
+   * @todo: Remove/Refactor in D8
+   */
+  if (!crossDomains) {
+    return false;
+  }
+  else {
+    return $.inArray(hostname, crossDomains) > -1 ? true : false;
+  }
+}
+
+})(jQuery);
+;
